fc7d522d0e1a3340114b946143a0b23fd214bdf5
hoangvuong.tran21 <hoangvuong.tran21@gmail.com>
2018-11-28 21:53:43 +0700

    Change X and O in Board

--- src/Game.js
+++ src/Game.js
@@ -2,17 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class Square extends Component {
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>{props.value}</button>
+  );
 }
 
 class Board extends Component {
@@ -20,15 +13,19 @@ class Board extends Component {
     super(props);
 
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     }
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
 
-    this.setState({squares});
+    this.setState({
+      squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -41,7 +38,7 @@ class Board extends Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
     return (
       <div>
